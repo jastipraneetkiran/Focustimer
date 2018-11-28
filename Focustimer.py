b5296074c4116bcb798bd6ec599475d27a8652ae
Jasti Praneet Kiran <40835644+jastipraneetkiran@users.noreply.github.com>
2018-11-28 19:11:01 +0530

    first commit

---
+++ Focustimer.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp=r"D:\Dropbox\pp\block_websites\Demo\hosts"
+#hosts_path="/etc/hosts"
+
+hosts_path="C:\\Windows\\System32\\drivers\\etc\\hosts"
+print(hosts_path)
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com","www.max.com","max.com","dell.com","www.dell.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,13) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23):
+        print("Working hours...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+"\t"+website+"\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...")
+    time.sleep(5)
